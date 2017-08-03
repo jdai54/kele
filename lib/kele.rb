44caf6dc86bc5ee16fb35448c43bac4f2ff9a1d8
Jake <jdai54@gmail.com>
2017-08-03 11:30:09 -0700

    getting nil for @auth_token

--- lib/kele.rb
+++ lib/kele.rb
@@ -8,8 +8,14 @@ class Kele
   def initialize(email, password)
     response = self.class.post(api_url("sessions"), body: { email: email, password: password })
     raise 'Invalid email or password' if response.code == 404
-    @auth_token = response["autho_token"]
+    @auth_token = response["auth_token"]
   end
+
+  def get_me
+    response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
+    @user = JSON.parse(response.body)
+  end
+
 
   private
 
